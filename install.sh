d44d55aff12cad324b1f862488038c5729d5e957
Chris Mytton <self@hecticjeff.net>
2011-05-30 20:31:20 +0100

    add install and upgrade scripts

---
+++ install.sh
@@ -0,0 +1,52 @@
+#/bin/sh
+set -e
+
+DOTFILE_DIR=${DOTFILE_DIR:-"$HOME/Dropbox/.dotfiles"}
+
+# dotfiles
+if [ ! -d "$DOTFILE_DIR" ]; then
+  echo "installing dotfiles"
+  git clone "https://github.com/hecticjeff/dotfiles" "$DOTFILE_DIR"
+fi
+
+# oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "installing oh-my-zsh"
+  git clone "https://github.com/$GITHUB_USER/oh-my-zsh" ~/.oh-my-zsh
+fi
+
+# janus
+if [ ! -f "$HOME/.vim/README.markdown" ]; then
+  echo "installing janus"
+  for i in ~/.vim ~/.vimrc ~/.gvimrc; do [ -e $i ] && mv $i $i.old; done
+  git clone "https://github.com/$GITHUB_USER/janus" ~/.vim
+  cd ~/.vim
+  rake
+fi
+
+# bin
+INSTALL_PREFIX="$HOME/local/shared/bin"
+if [ ! -d $INSTALL_PREFIX ]; then
+  mkdir -p $INSTALL_PREFIX
+fi
+
+# e.g `remote_install hub http://defunkt.io/hub/standalone`
+remote_install () {
+  curl $2 -sL > "$INSTALL_PREFIX/$1"
+  chmod +x "$INSTALL_PREFIX/$1"
+}
+
+# hub
+echo "installing hub"
+remote_install hub http://defunkt.io/hub/standalone
+
+# ack
+echo "installing ack"
+remote_install ack http://betterthangrep.com/ack-standalone
+
+# licence
+echo "installing licence"
+remote_install licence https://gist.github.com/raw/767068/ff15d3d44e54cbd0ad5ac72dc832e4c7193f5193/license
+
+# upgrade
+. "$DOTFILE_DIR/upgrade.sh"
---
+++ upgrade.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+set -e
+
+# Run the script from the dotfiles directory
+cd ~/Dropbox/.dotfiles
+
+echo "installing dotfiles"
+
+# global flags for installing dotfiles
+skip_all=false
+overwrite_all=false
+backup_all=false
+
+# loop over all the `.symlink` files
+for linkable in *.symlink
+do
+  # reset these to false at the start of each loop
+  overwrite=false
+  backup=false
+  skip=false
+
+  # file that we are symlinking to
+  file="`pwd`/$linkable"
+
+  # the target for the symlink
+  target="$HOME/.`basename $linkable .symlink`"
+
+  # check if the target already exists
+  if [ -f "$target" -o -L "$target" ]; then
+
+    # check that we're not already on an *all* flag
+    if ( ! $overwrite_all && ! $backup_all ); then
+
+      # ask the user how they want to proceed
+      echo -n "file exists $target [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all: "
+      read confirm
+
+      # check the user's choice
+      case "$confirm" in
+        o) overwrite=true ;;
+        b) backup=true ;;
+        s) skip=true ;;
+        O) overwrite_all=true ;;
+        B) backup_all=true ;;
+        S) skip_all=true ;;
+      esac
+    fi
+
+    # just exit the script if we're skipping all
+    if ( $skip_all ); then
+      echo "skipping all"
+      exit 0
+    fi
+
+    # continue to the next file if we're skipping this one
+    if ( $skip ); then
+      echo "skipping $target"
+      continue
+    fi
+
+    # move the file out of the way if the user wants to backup
+    if ( $backup || $backup_all ); then
+      echo "backing up $target"
+      mv "$target" "$target.backup"
+    elif ( $overwrite || $overwrite_all ); then
+      echo "removing $target"
+      rm -rf "$target"
+    fi
+  fi
+
+  # finally link in the file
+  echo "linking $target"
+  ln -s "$file" "$target"
+
+done
