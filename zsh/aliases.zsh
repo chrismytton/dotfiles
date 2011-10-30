ae80eb5f8de5023834931c63e6b3998660e47f39
Chris Mytton <self@hecticjeff.net>
2011-10-30 19:29:52 +0000

    Remove zsh update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,8 +4,6 @@ alias l='ls -laF'
 
 # Update and edit dotfiles
 alias reload!='. ~/.zshrc'
-alias zup='sh -c "$(curl -fsSL http://git.io/yS_hZw)"'
-alias zedit='$EDITOR ~/.dotfiles'
 
 # MIT-LICENCE
 # Usage `license > MIT-LICENSE`
