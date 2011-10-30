f013f5096c1f17ad50f76402d9c69a8eb8e9bab1
Chris Mytton <self@hecticjeff.net>
2011-10-30 15:53:16 +0000

    Add aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,7 @@
+# ls
+alias ll='ls -lF'
+alias l='ls -laF'
+
+# MIT-LICENCE
+# Usage `licence > MIT-LICENCE`
+alias licence='sh -c "$(curl -fsSL https://raw.github.com/gist/767068/770b334c0b74903344158650c165a0cb94ddbff5/license)"'
