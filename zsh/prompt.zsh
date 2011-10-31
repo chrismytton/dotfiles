2e15656daa71c2c835e25c1c4540eb1a18addb32
Chris Mytton <self@hecticjeff.net>
2011-10-31 18:50:51 +0000

    Better docs for prompt.zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,10 +2,13 @@
 autoload -U colors && colors
 setopt PROMPT_SUBST
 
-# prompt
+# For more information on the variables that can be used in the prompt,
+# and using color, see the archlinux zsh guide.
+#
+# https://wiki.archlinux.org/index.php/Zsh#Customizing_your_prompt.
 PROMPT='%~%(?..%{$fg[red]%})%#%{$reset_color%} '
 
-# extended ssh prompt
+# Show current user and host when using ssh.
 if [ "$SSH_CONNECTION" ]; then
   PROMPT="%n@%m:$PROMPT"
 fi
