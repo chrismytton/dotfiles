c3676f8e4dbbaf6b32e4158a3df75e195d85f295
Chris Mytton <self@hecticjeff.net>
2011-11-30 10:51:54 +0000

    Add git branch to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,11 +2,19 @@
 autoload -U colors && colors
 setopt PROMPT_SUBST
 
+# adds the current branch name in green
+git_prompt_info() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null)
+  if [[ -n $ref ]]; then
+    echo "[%{$fg_bold[green]%}${ref#refs/heads/}%{$reset_color%}]"
+  fi
+}
+
 # For more information on the variables that can be used in the prompt,
 # and using color, see the archlinux zsh guide.
 #
 # https://wiki.archlinux.org/index.php/Zsh#Customizing_your_prompt.
-PROMPT='%~%(?..%{$fg[red]%})%#%{$reset_color%} '
+PROMPT='%~$(git_prompt_info)%(?..%{$fg[red]%})%#%{$reset_color%} '
 
 # Show current user and host when using ssh.
 if [ "$SSH_CONNECTION" ]; then
