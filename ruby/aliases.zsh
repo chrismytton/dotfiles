8f56d716215d45e2c48ba3698a6ae35918710314
Chris Mytton <self@hecticjeff.net>
2013-08-11 10:50:06 +0100

    Use require command line flag for ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,2 +1,2 @@
-alias urlencode='ruby -e "require \"uri\"; puts URI.encode_www_form_component(ARGV[0])"'
-alias urldecode='ruby -e "require \"uri\"; puts URI.decode_www_form_component(ARGV[0])"'
+alias urlencode='ruby -r uri -e "puts URI.encode_www_form_component(ARGV[0])"'
+alias urldecode='ruby -r uri -e "puts URI.decode_www_form_component(ARGV[0])"'
