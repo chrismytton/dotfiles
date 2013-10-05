5b4d3dbd192e71ebf61ed7655f14a5ffd11a50af
Chris Mytton <self@hecticjeff.net>
2013-10-05 12:54:05 +0100

    npm installs to /usr/local/bin now

--- node/config.zsh
+++ node/config.zsh
@@ -1,8 +1,3 @@
 export NODE_PATH="/usr/local/lib/node_modules"
 
-# Add npm's bin to the path if it exists.
-if [ -d "/usr/local/share/npm/bin" ]; then
-  path=(/usr/local/share/npm/bin $path)
-fi
-
 if which npm > /dev/null; then . <(npm completion); fi
