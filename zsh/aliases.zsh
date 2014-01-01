5592867f8a823868ad1198547ea7abee054f4c27
Chris Mytton <self@hecticjeff.net>
2014-01-01 16:04:50 +0000

    Remove httpserver alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,11 +20,6 @@ alias _='sudo'
 # Reload dotfiles.
 alias reload!='. ~/.zshrc'
 
-# Usage: httpserver [<port>]
-httpserver() {
-  python -m SimpleHTTPServer "$@"
-}
-
 find-replace () {
   ag -l $1 | xargs sed -i '' -e "s/$1/$2/g"
 }
