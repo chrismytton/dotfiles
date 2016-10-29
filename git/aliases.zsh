1787340a2ba6f23a6b153f3d1223f301e4593153
Chris Mytton <chrismytton@users.noreply.github.com>
2016-10-29 11:35:19 +0100

    Add git plugin back in
    The oh-my-zsh one was a bit unweildy and I didn't use even 20% of it.

---
+++ git/aliases.zsh
@@ -0,0 +1,25 @@
+alias g=git
+alias gst='git status -sb'
+alias gp='git push'
+alias gpo='gp origin master'
+alias gps='gp stage'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gdca='git diff --cached'
+alias ga='git add'
+alias gcl='git config --list'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gx=gitx
+alias gl='git l'
+alias gpl='git pl'
+alias gr='git remote -v'
+alias gfp='git fetch --all --prune'
+alias gg='git grep'
+alias gmom='git merge origin master'
+
+# hub
+if which hub > /dev/null; then function git(){hub "$@"} fi
