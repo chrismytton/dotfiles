2b2e54abf7ac19709c85fcee70cc751c6edc849d
Chris Mytton <chrismytton@gmail.com>
2016-11-12 21:31:20 +0100

    Clean up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,8 @@
 alias v='vim'
-alias ls="ls -G"
+alias ls='ls -G'
 alias l='ls -laFh'
-alias tz='sudo systemsetup -settimezone'
-alias fixcamera="sudo killall VDCAssistant"
+alias tz='systemsetup -settimezone'
+alias fixcamera='killall VDCAssistant'
 alias be='bundle exec'
 alias bi='bundle install'
 alias bu='bundle update'
